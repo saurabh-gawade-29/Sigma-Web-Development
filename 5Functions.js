44c7045596cebd1548f88e2d83ab947e63df541e
Saurabh Gawade <saurabhgawade2920@gmail.com>
2024-04-07 19:36:45 +0530

    update arrow function

--- 5Functions.js
+++ 5Functions.js
@@ -1,4 +1,4 @@
-//! Functions
+//! Functions -  Reusablity
 function greet(name) {
     console.log("Hey " + name + " You are nice");
 }
@@ -20,3 +20,22 @@ function summ(c, d) {
 }
 let result = summ(3, 6);
 console.log("Result is: " + result);
+
+//! Default Parameter
+function sumThree(e, f, g = 5) {
+    return e + f + g
+}
+
+let resultThree = sumThree(5, 5); //g=5
+let resultThreeWithG = sumThree(5, 5, 6); //g=5
+console.log(resultThree, "Sum of three number");
+console.log(resultThreeWithG, "Sum of three number with G");
+
+//! Arrow Function - ananymous function - direct return without {}
+//* Sum Of two number using arrow Function
+let arrowFunction = (h, i) => h + i;
+console.log(arrowFunction(5, 5));
+
+//! Arrow Function - without () and without {}
+let anotherArrowFunction = a => a;
+console.log(anotherArrowFunction(5));
