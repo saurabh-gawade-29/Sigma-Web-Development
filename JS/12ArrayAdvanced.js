d89518328730e940b5fb1c38b056c549748d5c4d
saurabh gawade <saurabhgawade@finacus.co.in>
2024-06-11 10:23:18 +0530

    update

--- JS/12ArrayAdvanced.js
+++ JS/12ArrayAdvanced.js
@@ -1,8 +1,10 @@
+//* it's return new Array
 //! MAP
 let arr = [1, 2, 3, 4, 5, 6, 7, 8];
 let newArr = arr.map((element) => element ** 2);
 console.log(newArr);
 
+//* It's return new Array
 //! Filter
 function greaterThanFive(e) {
   if (e > 5) {
@@ -11,6 +13,11 @@ function greaterThanFive(e) {
   return false;
 }
 let newFilterArr = arr.filter(greaterThanFive);
-let newFilterArrShort = arr.filter((e)=> e > 5 ? true : false );
+let newFilterArrShort = arr.filter((e) => (e > 5 ? true : false));
 console.log(newFilterArr);
 console.log(newFilterArrShort);
+
+//! Reduce Method
+let redFunc = (a, b) => a * b;
+const newArray = arr.reduce(redFunc);
+console.log(newArray, "newArray");
